04816b47e11c4dca0a89ddfcaa87227dd90d791c
Pietro Di Giovanni <pietro.digiovanni.dev@gmail.com>
2021-07-03 19:07:18 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-console.log("Hello World!");
-
 var newTaskInput = document.getElementById('new__task-input');
 var newTaskButton = document.getElementById('new__task-button');
 var taskContainer = document.getElementById('task__container');
-var taskContent = document.getElementById('task');
-
+var taskContent = document.getElementById('task__content');
 
 newTaskButton.addEventListener('click', e => {
     e.stopPropagation();
 
-    if (newTaskInput.value.length != 0) {
-        console.log(newTaskInput.value);
-    } else 
-    console.log("Inserisci testo")
+    if (newTaskInput.value.length === 0 || !newTaskInput.value.replace(/\s/g, '').length) {
+        console.log("Inserisci testo");
 
+        newTaskInput.classList.add('input--error');
+    } else {
+
+        taskContainer.innerHTML += '<li id="task">' +
+            '<div class="task--content">' +
+            '<p class="task--body">' + newTaskInput.value.trim() + '</p>' +
+            '</div>' +
+            '</li>';
+
+        newTaskInput.classList.remove('input--error');
+        newTaskInput.value = '';
+    }
 
 })
