901e5dc8fe1e2639f716d49f49dc8d4e420ddd91
Pietro Di Giovanni <pietro.digiovanni.dev@gmail.com>
2021-07-09 17:18:45 +0200

    javascripts added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
 var newTaskInput = document.getElementById('new__task-input');
 var newTaskButton = document.getElementById('new__task-button');
 var taskContainer = document.getElementById('task__container');
-var taskContent = document.getElementById('task__content');
+var taskListEmpty = document.getElementById('task__list-empty');
 
-newTaskButton.addEventListener('click', e => {
-    e.stopPropagation();
+var task = document.getElementById('task--');
+var taskIconCheck = document.getElementById('task__checked');
+
+var taskNum = 0;
+
+newTaskButton.addEventListener('click', newTask);
+newTaskInput.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+        newTask();
+    }
+});
+
+function newTask() {
 
     if (newTaskInput.value.length === 0 || !newTaskInput.value.replace(/\s/g, '').length) {
-        console.log("Inserisci testo");
-
         newTaskInput.classList.add('input--error');
-    } else {
 
-        taskContainer.innerHTML += '<li id="task">' +
+    } else {
+        //Add task on top of the <ul>
+        taskContainer.insertAdjacentHTML('afterbegin', '<li id="task--' + taskNum + '">' +
             '<div class="task--content">' +
+            '<span class="material-icons icon--task-check" id="task__checked">' +
+            'radio_button_unchecked' +
+            '</span>' +
             '<p class="task--body">' + newTaskInput.value.trim() + '</p>' +
             '</div>' +
-            '</li>';
+            '</li>');
 
         newTaskInput.classList.remove('input--error');
         newTaskInput.value = '';
     }
 
-})
+    taskNum++;
+
+    if (taskContainer.children.length > 0) {
+        taskListEmpty.style.display = 'none';
+    } else {
+        taskListEmpty.style.display = 'block';
+    }
+}
+
+if (taskIconCheck) {
+    taskIconCheck.addEventListener('click', completeTask);
+
+}
+
+function completeTask() {
+    taskIconCheck.innerHTML = 'radio_button_check';
+}
+
+function removeTask() {
+
+    idTask = task + taskNum
+    console.log(idTask)
+}
